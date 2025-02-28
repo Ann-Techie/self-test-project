d8ff0d14af047c23e40fe3b2f807517bbb321b74
Ann Kamau <amuringok@gmail.com>
2025-02-28 16:34:33 +0300

    added DOM manipulation on HTML  search engine

--- src/script.js
+++ src/script.js
@@ -1,2 +1,19 @@
+function displaySearchCity(event) {
+  event.preventDefault();
+  let searchInputCity = document.querySelector("#city-searched");
+  let inputCityElement = document.querySelector("#input-city");
+  inputCityElement.innerHTML = searchInputCity.value;
+}
+
+function displayWeather(response) {
+  console.log(response.data);
+}
+
+let city = "Nairobi";
+let apiKey = "5765tb49aco10f17ace1b436b0213fc4";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+
+axios.get(apiUrl).then(displayWeather);
+
 let searchInput = document.querySelector("#search-form-input");
-console.log(searchInput);
+searchInput.addEventListener("submit", displaySearchCity);
